7270d38dbd3a1c380fe79085024d7eebb11b3162
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-08-31 03:47:11 -0700

    fix return bugs

--- car_parts/batteries.py
+++ car_parts/batteries.py
@@ -5,16 +5,6 @@ class Battery(Car):
     pass
 
 
-class SpindlerBattery(Battery):
-    def __init__(self, last_service_date, current_date):
-        self.current_date = current_date
-        self.last_service_date = last_service_date
-
-    def needs_service(self):
-        service_threshold = 365 * 2
-        return self.current_date - self.last_service_date >= service_threshold
-
-
 class NubbinBattery(Battery):
     def __init__(self, last_service_date, current_date):
         self.last_service_date = last_service_date
@@ -22,4 +12,14 @@ class NubbinBattery(Battery):
 
     def needs_service(self):
         service_threshold = 365 * 4
-        return self.current_date - self.last_service_date >= service_threshold
+        return (self.current_date - self.last_service_date).days >= service_threshold
+
+
+class SpindlerBattery(Battery):
+    def __init__(self, last_service_date, current_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        service_threshold = 365 * 2
+        return (self.current_date - self.last_service_date).days >= service_threshold
