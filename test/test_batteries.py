6af9ff7b5c28a5a1d9e5e255119e8368e799810c
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-08-31 04:47:42 -0700

    Complete Task 3

--- test/test_batteries.py
+++ test/test_batteries.py
@@ -5,14 +5,7 @@ from car_parts.batteries import NubbinBattery, SpindlerBattery
 
 
 class TestNubbinBattery(unittest.TestCase):
-    def test_battery_should_be_serviced_4_yrs(self):
-        current_date = datetime.today().date()
-        last_service_date = current_date.replace(year=current_date.year - 4)
-
-        battery = NubbinBattery(last_service_date, current_date)
-        self.assertTrue(battery.needs_service())
-
-    def test_battery_should_be_serviced_5_yrs(self):
+    def test_battery_should_be_serviced(self):
         current_date = datetime.today().date()
         last_service_date = current_date.replace(year=current_date.year - 5)
 
@@ -28,13 +21,6 @@ class TestNubbinBattery(unittest.TestCase):
 
 
 class TestSpindlerBattery(unittest.TestCase):
-    def test_battery_should_be_serviced_2_yrs(self):
-        current_date = datetime.today().date()
-        last_service_date = current_date.replace(year=current_date.year - 2)
-
-        battery = SpindlerBattery(last_service_date, current_date)
-        self.assertTrue(battery.needs_service())
-
     def test_battery_should_be_serviced_3_yrs(self):
         current_date = datetime.today().date()
         last_service_date = current_date.replace(year=current_date.year - 3)
